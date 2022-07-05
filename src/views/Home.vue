c6e9ad2aefeb7d9c0959e0879d24f55468245eb6
“chengqingyuan git config --global user.name “chengqingyuan <2033619233@qq.com>
2022-07-05 10:09:09 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,8 +11,8 @@ import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
+  components:   {
     HelloWorld
   }
 })
-</script>
+</script> 
